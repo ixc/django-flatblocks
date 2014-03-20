4580d38bc12a2c3aa3cd9c8f1a348d5bc4a2cfbb
Tai Lee <real.human@mrmachine.net>
2014-03-20 17:12:33 +1100

    Remove broken backwards compatibility for deprecated `truncate_words`.
    The `Truncator` class has been available since Django 1.4, so we should
    be able to just use that instead of `truncate_words`, unless we really
    need to still be supporting Django 1.3
    
    The `Truncator` class isn't a drop-in replacement anyway, so this code
    wasn't working on Django 1.4 and up.

--- flatblocks/admin.py
+++ flatblocks/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from django.utils.html import strip_tags
-try:
-    from django.utils.text import Truncator as truncate_words
-except ImportError:
-    from django.utils.text import truncate_words
+from django.utils.text import Truncator
 from django.template.defaultfilters import striptags
 
 from flatblocks.models import FlatBlock
@@ -19,7 +16,7 @@ class FlatBlockAdmin(admin.ModelAdmin):
     actions = None
 
     def content_summary(self, block):
-        return truncate_words(strip_tags(block.content), 25)
+        return Truncator(strip_tags(block.content)).words(25)
 
     def has_add_permission(self, request):
         return False
